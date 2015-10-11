b69dcb9b59f68cdb118e0a033106bd79cd57f674
abc <abc@abc-PC>
2015-10-11 14:50:00 +0900

    시작 인터페이스 수정

--- src/AnswerRunner.java
+++ src/AnswerRunner.java
@@ -5,9 +5,14 @@ public class AnswerRunner implements Runnable {
 	String answer="";
 	@Override
 	public void run() {
-		System.out.println("사용 봅");
+		
+		System.out.println("-------------------------------");
+		System.out.println("   환영합니다.청기백기 게임을 시작합니다!");
+		System.out.println("-------------------------------");
+		System.out.println("조작키 설명");
 		System.out.println("1.질의에 맞는 숫자를 입력해주세요");
-		System.out.println("2. 0번 둘다올려, 1번 청기올려, 2번 백기올려 3번 백기내려 4번 청기내");
+		System.out.println("2. 0번 둘다올려, 1번 청기올려, 2번 백기올려 3번 백기내려 4번 청기내려");
+		
 		while(true) {
 			answer = scan.next();
 			if("q".equals(answer)) System.exit(0);
