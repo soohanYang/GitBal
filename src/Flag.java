b167a1f754c2f59323b0a137de92ead9c253c16a
abc <abc@abc-PC>
2015-10-11 12:57:24 +0900

    질의문 생성

--- src/Flag.java
+++ src/Flag.java
@@ -1,9 +1,9 @@
 
 public class Flag {
+	public static String[] question = {"청기올려", "백기올려", "청기올리지 말고 백기 올려",
+		"청기올리지 말고 백기 내려", "백기올리지말고 청기 올려", "백기올리지말고 청기 내려", "둘다 올려"};
+	public static String[] answer = {"1", "2", "2", "4", "1", "5", "0"};
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
+
