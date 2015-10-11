3c4d631105f5295974417b0ac45f4fe69eceb666
abc <abc@abc-PC>
2015-10-11 14:16:55 +0900

    사용 설명서 입력

--- src/AnswerRunner.java
+++ src/AnswerRunner.java
@@ -5,13 +5,16 @@ public class AnswerRunner implements Runnable {
 	String answer="";
 	@Override
 	public void run() {
+		System.out.println("사용 봅");
+		System.out.println("1.질의에 맞는 숫자를 입력해주세요");
+		System.out.println("2. 0번 둘다올려, 1번 청기올려, 2번 백기올려 3번 백기내려 4번 청기내");
 		while(true) {
 			answer = scan.next();
 			if("q".equals(answer)) System.exit(0);
 			if(Flag.answer[Flag.value].equals(answer)) {
-				System.out.println("딩동댕!! 정답!!");
+				System.out.println("딩동댕.. 정답!!");
 			}else {
-				System.out.println("땡!! 틀렸습니다!");
+				System.out.println("땡.. 틀렸습니다!");
 			}
 			synchronized (this) {
 				if(Flag.isWait) {
--- src/Flag.java
+++ src/Flag.java
@@ -2,7 +2,7 @@
 public class Flag {
 	public static String[] question = {"청기올려", "백기올려", "청기올리지 말고 백기 올려",
 		"청기올리지 말고 백기 내려", "백기올리지말고 청기 올려", "백기올리지말고 청기 내려", "둘다 올려"};
-	public static String[] answer = {"1", "2", "2", "4", "1", "5", "0"};
+	public static String[] answer = {"1", "2", "2", "3", "1", "4", "0"};
 
 	public static int value;
 	public static boolean isWait;
